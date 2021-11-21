804e77f038ec99291b5093d0dbdd30c1157d980b
Miriosama <miriosama+github@protonmail.com>
2021-11-21 11:11:33 +0000

    testnet

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Application } from "express";
-import usersRouter from "./api/controllers/users/router";
-import nftsRouter from "./api/controllers/nfts/router";
 import categoriesRouter from "./api/controllers/categories/router";
 import followRouter from "./api/controllers/follows/router";
+import nftsRouter from "./api/controllers/nfts/router";
+import usersRouter from "./api/controllers/users/router";
 
 export default function routes(app: Application): void {
   app.use("/api/users", usersRouter);
   app.use("/api/NFTs", nftsRouter);
   app.use("/api/categories", categoriesRouter);
-  app.use("/api/follow", followRouter)
+  app.use("/api/follow", followRouter);
+  app.use("/ping", (req, res) => {
+    res.json("Working");
+  });
 }
